11e0e976c6f76fd63d0ce7dbd33b6c0d2dc839b1
cyf112233 <frank13967973981@163.com>
2025-08-31 15:36:26 +0800

    Update VulkanZinkRenderer.kt

--- ZalithLauncher/src/main/java/com/movtery/zalithlauncher/renderer/renderers/VulkanZinkRenderer.kt
+++ ZalithLauncher/src/main/java/com/movtery/zalithlauncher/renderer/renderers/VulkanZinkRenderer.kt
@@ -11,8 +11,20 @@ class VulkanZinkRenderer : RendererInterface {
 
     override fun getRendererEnv(): Lazy<Map<String, String>> = lazy {
         mapOf(
+            "GALLIUM_DRIVER" to "zink",
+            "MESA_LOADER_DRIVER_OVERRIDE" to "zink",
+            "POJAV_RENDERER" to "opengles3_desktopgl_zink_kopper",
+            "LIBGL_ES" to "4",
             "MESA_GL_VERSION_OVERRIDE" to "4.6",
-            "MESA_GLSL_VERSION_OVERRIDE" to "460"
+            "MESA_GLSL_VERSION_OVERRIDE" to "460",
+            "force_glsl_extensions_warn" to "true",
+            "allow_glsl_extension_directive_midshader" to "true",
+            "allow_higher_compat_version" to "true",
+            "LIB_MESA_NAME" to "libEGL_mesa.so",
+            "vblank_mode" to "0",
+            "radeonsi_disable_sync_shaders" to "1",
+            "MESA_DEBUG" to "1",
+            "ZINK_DEBUG" to "validate"
         )
     }
 
@@ -23,8 +35,10 @@ class VulkanZinkRenderer : RendererInterface {
             "libsync.so",
             "libglxshim.so",
             "libgallium_dri.so",
-            "libEGL_mesa.so"
+            "libEGL_mesa.so",
+            "libvulkan.so"
         )
     }
+
     override fun getRendererLibrary(): String = "libEGL_mesa.so"
 }
